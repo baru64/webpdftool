507cd1d48af826ac6bb83f8e8cba63bd587ca01c
Bartosz Krakowiak <bkrakowiak@man.poznan.pl>
2019-04-18 08:58:54 +0200

    Add list reorder

--- converter.js
+++ converter.js
@@ -1,5 +1,5 @@
 var uploadedFiles = Array();
-var uploadedFilesCount = 0;
+var uploadedFilenames = Array(); // [filename, id]
 var convertedFile = "test";
 
 async function add() {
@@ -9,22 +9,23 @@ async function add() {
     for(let i = 0; i < files.length; i++) {
         let file = files.item(i);
         let li = document.createElement("li");
+        let uploadedFilesCount = uploadedFilenames.length;
         li.setAttribute("id", "file-" + uploadedFilesCount);
         
         let buttonUp = document.createElement("button");
-        buttonUp.setAttribute("id", "fileUp-" + uploadedFilesCount);
-        buttonUp.setAttribute("onclick", 'swap(this, "up")');
+        buttonUp.setAttribute("class", "file-" + uploadedFilesCount);
+        buttonUp.setAttribute("onclick", 'reorder(this, "up")');
         buttonUp.append("↑");
         let buttonDown = document.createElement("button");
-        buttonDown.setAttribute("id", "fileDown-" + uploadedFilesCount);
-        buttonDown.setAttribute("onclick", 'swap(this, "down")');
+        buttonDown.setAttribute("class", "file-" + uploadedFilesCount);
+        buttonDown.setAttribute("onclick", 'reorder(this, "down")');
         buttonDown.append("↓");
 
         li.append(file.name);
         li.appendChild(buttonUp);
         li.appendChild(buttonDown);
         fileList.appendChild(li);
-        uploadedFilesCount++;
+        uploadedFilenames.push([file.name, "file-"+uploadedFilesCount]);
 
         let fileBuffer;
         let reader = new FileReader();
@@ -38,13 +39,53 @@ async function add() {
     }
 }
 
-function swap(element, direction) { // TODO
+function reorder(element, direction) { // TODO
+    let fileList = document.getElementById("file-list");
+    let i = 0;
+    while(uploadedFilenames[i][1] != element.getAttribute("class")) {
+        i++;
+        if(i == uploadedFilenames.length)
+            return;
+    }
+    console.log("----- index: " + i);
+    if(direction == "up" && i != 0) {
+        swap(uploadedFilenames, i, i-1);
+        swap(uploadedFiles, i, i-1);
+    } else if (direction == "down" && i != uploadedFilenames.length)
+    {
+        swap(uploadedFilenames, i, i+1);
+        swap(uploadedFiles, i, i+1);
+    }
+    fileList.innerHTML = "";
+    for(let k = 0; k < uploadedFilenames.length; k++) {
+        let li = document.createElement("li");
+        li.setAttribute("id", uploadedFilenames[k][1]);
+        
+        let buttonUp = document.createElement("button");
+        buttonUp.setAttribute("class", uploadedFilenames[k][1]);
+        buttonUp.setAttribute("onclick", 'reorder(this, "up")');
+        buttonUp.append("↑");
+        let buttonDown = document.createElement("button");
+        buttonDown.setAttribute("class", uploadedFilenames[k][1]);
+        buttonDown.setAttribute("onclick", 'reorder(this, "down")');
+        buttonDown.append("↓");
 
+        li.append(uploadedFilenames[k][0]);
+        li.appendChild(buttonUp);
+        li.appendChild(buttonDown);
+        fileList.appendChild(li);
+    }
+}
+
+function swap(arr, indexA, indexB) {
+    let tmp = arr[indexA];
+    arr[indexA] = arr[indexB];
+    arr[indexB] = tmp;
 }
 
 function reset() {
     uploadedFiles = Array();
-    uploadedFilesCount = 0;
+    uploadedFilenames = Array();
     let fileList = document.getElementById("file-list");
     while (fileList.firstChild) {
         fileList.removeChild(fileList.firstChild);
